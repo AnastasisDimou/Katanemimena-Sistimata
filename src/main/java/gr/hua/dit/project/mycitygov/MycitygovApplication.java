7e59ed14665244e3b1fcc6e5521918de9bc779ac
AnastasisDimou <andigli17@gmail.com>
2025-11-24 21:03:56 +0200

    Initial commit

---
+++ src/main/java/gr/hua/dit/project/mycitygov/MycitygovApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.project.mycitygov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycitygovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycitygovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/hua/dit/project/mycitygov/MycitygovApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.project.mycitygov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycitygovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
