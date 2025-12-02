5774ce870feb08db292f99ba44e2577522bb5c9a
AnastasisDimou <andigli17@gmail.com>
2025-12-02 22:51:39 +0200

    Changed file name

---
+++ src/main/java/gr/hua/dit/project/mycitygov/core/port/repository/ServiceDepartmentRepository.java
@@ -0,0 +1,20 @@
+package gr.hua.dit.project.mycitygov.core.port.repository;
+
+import gr.hua.dit.project.mycitygov.core.model.ServiceDepartment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ServiceDepartmentRepository extends JpaRepository<ServiceDepartment, Long> {
+   Optional<ServiceDepartment> findByCode(String code);
+
+   Optional<ServiceDepartment> findByName(String name);
+
+   boolean existsByCode(String code);
+
+   boolean existsByName(String name);
+
+}
