1284e9646c1067dfe9b047e673df4e16ba4fcccc
AnastasisDimou <andigli17@gmail.com>
2025-12-05 21:13:06 +0200

    Fixed error with passwordEncoder

--- src/main/java/gr/hua/dit/project/mycitygov/config/SecurityConfig.java
+++ src/main/java/gr/hua/dit/project/mycitygov/config/SecurityConfig.java
@@ -3,20 +3,28 @@ package gr.hua.dit.project.mycitygov.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-   @Bean
-   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-      http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                  .anyRequest().permitAll())
-            .formLogin(form -> form.disable())
-            .httpBasic(basic -> basic.disable());
+      @Bean
+      public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                        .csrf(csrf -> csrf.disable())
+                        .authorizeHttpRequests(auth -> auth
+                                    .anyRequest().permitAll())
+                        .formLogin(form -> form.disable())
+                        .httpBasic(basic -> basic.disable());
 
-      return http.build();
-   }
+            return http.build();
+      }
+
+      @Bean
+      public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+      }
+
 }
