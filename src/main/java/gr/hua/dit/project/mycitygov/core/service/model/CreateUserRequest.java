fd15b3316ea8ba1d6e3d68eca062a60366925e0b
AnastasisDimou <andigli17@gmail.com>
2025-12-17 18:41:29 +0200

    Commit to pull

--- src/main/java/gr/hua/dit/project/mycitygov/core/service/model/CreateUserRequest.java
+++ src/main/java/gr/hua/dit/project/mycitygov/core/service/model/CreateUserRequest.java
@@ -9,17 +9,17 @@ import jakarta.validation.constraints.Size;
  */
 public record CreateUserRequest(
 
-        @NotBlank @Size(min = 9, max = 9) String afm,
+                @NotBlank @Size(min = 9, max = 9) String afm,
 
-        @NotBlank @Size(min = 11, max = 11) String amka,
+                @NotBlank @Size(min = 11, max = 11) String amka,
 
-        @NotBlank @Size(max = 100) String firstName,
+                @NotBlank @Size(max = 100) String firstName,
 
-        @NotBlank @Size(max = 100) String lastName,
+                @NotBlank @Size(max = 100) String lastName,
 
-        @NotBlank @Email @Size(max = 100) String email,
+                @NotBlank @Email @Size(max = 100) String email,
 
-        @NotBlank @Size(min = 10, max = 15) String phoneNumber,
+                @NotBlank @Size(min = 10, max = 15) String phoneNumber,
 
-        @NotBlank @Size(min = 8, max = 64) String rawPassword) {
+                @NotBlank @Size(min = 8, max = 64) String rawPassword) {
 }
