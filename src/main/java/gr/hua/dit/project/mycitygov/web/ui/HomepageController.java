2f1ba1de64f93d71753c65fed1700fccac7f4bb8
AnastasisDimou <andigli17@gmail.com>
2026-01-03 21:20:54 +0200

    Minor gui changes

--- src/main/java/gr/hua/dit/project/mycitygov/web/ui/HomepageController.java
+++ src/main/java/gr/hua/dit/project/mycitygov/web/ui/HomepageController.java
@@ -1,13 +1,36 @@
 package gr.hua.dit.project.mycitygov.web.ui;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import gr.hua.dit.project.mycitygov.core.service.AppointmentService;
+import gr.hua.dit.project.mycitygov.core.service.RequestService;
+import gr.hua.dit.project.mycitygov.core.service.model.AppointmentView;
+import gr.hua.dit.project.mycitygov.core.service.model.TicketListItem;
+import org.springframework.security.core.Authentication;
 
 @Controller
 public class HomepageController {
 
+   private final AppointmentService appointmentService;
+   private final RequestService requestService;
+
+   public HomepageController(final AppointmentService appointmentService, final RequestService requestService) {
+      this.appointmentService = appointmentService;
+      this.requestService = requestService;
+   }
+
    @GetMapping("/")
-   public String showHomepage() {
+   public String showHomepage(final Authentication authentication, final Model model) {
+      if (AuthUtils.isAuthenticated(authentication)) {
+         final List<TicketListItem> tickets = this.requestService.getTicketsForCurrentUser();
+         final List<AppointmentView> appointments = this.appointmentService.listForCurrentUser();
+         model.addAttribute("ticketPreview", tickets.stream().limit(3).toList());
+         model.addAttribute("appointmentPreview", appointments.stream().limit(3).toList());
+      }
       return "homepage";
    }
 }
